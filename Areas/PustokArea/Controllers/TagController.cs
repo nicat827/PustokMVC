1d75910ee3ababae091de6c8d6243ca3f7a5b4e1
Zulfiyya Qurbanova <tu7u71zbq@code.edu.az>
2023-11-22 18:38:09 +0400

    CRUD for Tags added

---
+++ Areas/PustokArea/Controllers/TagController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PustokApp.DAL;
+using PustokApp.Models;
+
+namespace PustokApp.Areas.PustokArea.Controllers
+{
+    [Area("PustokArea")]
+
+    public class TagController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TagController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Tag> Tags = await _context.Tags
+                .Include(t => t.BookTags)
+                .ToListAsync();
+            return View(Tags);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Tag tag)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+
+            }
+
+            bool isExist = await _context.Tags.AnyAsync(t => t.Name == tag.Name);
+
+            if (isExist)
+            {
+                ModelState.AddModelError("Name", "This Tag already existed!");
+                return View();
+            }
+
+            await _context.Tags.AddAsync(tag);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            if (id <= 0) return BadRequest();
+            Tag tag = await _context.Tags.FirstOrDefaultAsync(t => t.Id == id);
+            if (tag is null) return NotFound();
+            return View(tag);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, Tag newTag)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            Tag oldTag = await _context.Tags.FirstOrDefaultAsync(t => t.Id == id);
+            if (oldTag is null) return NotFound();
+            bool isExistName = await _context.Tags.AnyAsync(t => t.Name.Trim() == newTag.Name.Trim() && t.Id != id);
+            if (isExistName)
+            {
+                ModelState.AddModelError("Name", "Tag with this name already exist!");
+                return View();
+            }
+
+
+            if (oldTag.Name == newTag.Name.Trim()) return RedirectToAction(nameof(Index));
+
+            oldTag.Name = newTag.Name;
+            await _context.SaveChangesAsync();
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id <= 0) return BadRequest();
+
+            Tag tag = await _context.Tags.FirstOrDefaultAsync(t => t.Id == id);
+
+            if (tag is null) return NotFound();
+
+            _context.Tags.Remove(tag);
+            _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- Models/Tag.cs
+++ Models/Tag.cs
@@ -1,12 +1,17 @@
-﻿namespace PustokApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PustokApp.Models
 {
     public class Tag
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Input cant be null!")]
+        [MaxLength(25, ErrorMessage = "Length cant be longer than 25!")]
+
         public string Name { get; set; }
 
-        public List<BookTag> BookTags { get; set; }
+        public List<BookTag>? BookTags { get; set; }
 
 
     }
